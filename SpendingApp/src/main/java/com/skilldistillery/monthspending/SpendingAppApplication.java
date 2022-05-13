2d43bba5c6c2e9f8a68b28be524ef24b1b26a04e
Angel <angel1179008@gmail.com>
2022-05-13 16:50:18 -0600

    Create boot MVC project, add JPA project dependencies

---
+++ SpendingApp/src/main/java/com/skilldistillery/monthspending/SpendingAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.monthspending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingAppApplication.class, args);
+	}
+
+}
---
+++ SpendingApp/src/test/java/com/skilldistillery/monthspending/SpendingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.monthspending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
